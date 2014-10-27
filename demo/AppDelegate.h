17d81f7df5e1ac09258230790a0b093f938b0df9
kiranjith <kiranjith@kiranjiths-iMac.local>
2014-10-26 23:49:54 -0700

    Initial Commit

---
+++ demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by kiranjith on 26/10/14.
+//  Copyright (c) 2014 kiranjith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo
+//
+//  Created by kiranjith on 26/10/14.
+//  Copyright (c) 2014 kiranjith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
